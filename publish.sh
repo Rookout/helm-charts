b2ae38c5606c38dc368e112dfb82f18bc94f017d
Gilad Weiss <gilad@rookout.com>
2020-08-04 12:08:47 +0300

    Fix up versions

--- publish.sh
+++ publish.sh
@@ -41,8 +41,8 @@ find "$HELM_CHARTS_SOURCE" -mindepth 1 -maxdepth 1 -type d | while read chart; d
   chart_version=$(cat $chart/Chart.yaml | grep -oE "version:\s[0-9]+\.[0-9]+\.[0-9]+" | grep -oE "[0-9]+\.[0-9]+\.[0-9]+")
   echo ">>> checking if version is already published"
   if [ -f "$chart_name/$chart_name-$chart_version.tgz" ]; then
-    echo ">>> VERSION $chart_version ALREADY EXISTS! Skipping..."
-    continue
+    echo ">>> Error: VERSION $chart_version ALREADY EXISTS! Update chart version."
+    exit 1
   else
     echo ">>> chart version is valid, continuing..."
   fi
