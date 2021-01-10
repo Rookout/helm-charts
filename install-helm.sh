36c2ce82543db62338af3055553739a49ef8c63c
adi <adi@rookout.com>
2021-01-10 17:54:33 +0200

    Fix stable repo url

--- install-helm.sh
+++ install-helm.sh
@@ -16,4 +16,4 @@ tar -zxf "helm-v${HELM_VERSION}-linux-amd64.tar.gz"
 chmod +x linux-amd64/helm
 alias helm=/tmp/helm/bin/linux-amd64/helm
 helm version -c
-helm init -c
+helm init -c --stable-repo-url https://charts.helm.sh/stable
