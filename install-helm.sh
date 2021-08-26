1247551d55ce969d28cf132e903883f3e373d5f0
Gilad Weiss <gilad@rookout.com>
2021-08-26 15:06:26 +0300

    Update helm bin location (#44)

--- install-helm.sh
+++ install-helm.sh
@@ -11,7 +11,7 @@ apk add ca-certificates git openssh
 
 echo '>> Installing Helm...'
 cd /tmp/helm/bin
-wget "https://storage.googleapis.com/kubernetes-helm/helm-v${HELM_VERSION}-linux-amd64.tar.gz"
+wget "https://get.helm.sh/helm-v${HELM_VERSION}-linux-amd64.tar.gz"
 tar -zxf "helm-v${HELM_VERSION}-linux-amd64.tar.gz"
 chmod +x linux-amd64/helm
 alias helm=/tmp/helm/bin/linux-amd64/helm
