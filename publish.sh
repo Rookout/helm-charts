479c889cbf9a0290a8fe769348d8bc9a0043c461
Gabriel Cohen <gavriel@rookout.com>
2021-11-29 23:18:26 +0200

    fixing publish.sh and publish job on circleCI

--- publish.sh
+++ publish.sh
@@ -9,31 +9,6 @@ WORKING_DIRECTORY="$PWD"
   exit 1
 }
 
-[ "$CIRCLE_PULL_REQUEST" ] || {
-  echo "ERROR: Environment variable CIRCLE_PR_NUMBER is required"
-  exit 1
-}
-
-# formating to number only
-PR_NUMBER=$(echo $CIRCLE_PULL_REQUEST | tr -dc '0-9')
-
-# Get labels from github-api and deserialize response using jq and sed
-LABELS=$(curl -s 'https://api.github.com/repos/'"${GITHUB_PAGES_REPO}"'/issues/'"${PR_NUMBER}"'/labels' | jq -r '.[] | .name' | sed 's/do not merge/do_not_merge/g') || {
-  echo "ERROR: curl failed to get response from github-api  /  failed to serialize data"
-  exit 1
-}
-
-if [ -z "$LABELS" ]; then
-  echo "ERROR: Github-api failed to return answer / no labels found"
-  exit 1 
-fi
-
-# Using regex to detect if at least one proper label exist 
-if ! [[ "$LABELS" =~ .*"controller".* || "$LABELS" =~ .*"datastore".* || "$LABELS" =~ .*"operator".* || "$LABELS" =~ .*"global_change"*. ]]; then
-  echo "ERROR: Github-api failed to return answer / no proper labels found"
-  exit 1
-fi
-
 [ -z "$GITHUB_PAGES_BRANCH" ] && GITHUB_PAGES_BRANCH=gh-pages
 [ -z "$HELM_CHARTS_SOURCE" ] && HELM_CHARTS_SOURCE="$WORKING_DIRECTORY/charts"
 [ -d "$HELM_CHARTS_SOURCE" ] || {
@@ -46,8 +21,6 @@ fi
   exit 1
 }
 
-echo "LABELS=$LABELS"
-echo "PR_NUMBER=$PR_NUMBER"
 echo "GITHUB_PAGES_REPO=$GITHUB_PAGES_REPO"
 echo "GITHUB_PAGES_BRANCH=$GITHUB_PAGES_BRANCH"
 echo "HELM_CHARTS_SOURCE=$HELM_CHARTS_SOURCE"
