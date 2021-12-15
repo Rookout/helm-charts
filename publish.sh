0d78c9fd6915b0267e9837e1a2b4cbbcd84300b7
Gabriel Cohen <94457453+Goobensio@users.noreply.github.com>
2021-12-15 11:38:01 +0200

    RK-11043 - adding github actions workflows + changing scripts to run on ubuntu (#54)
    * adding github actions workflows + changing scripts to run on ubuntu
    
    * switched cd directory to helm-charts instead of test
    
    * removed test variable + changed name of yaml file
    
    * added actions
    
    * updated helm version to 2.16.7
    
    * dummy commit
    
    * Revert to commit: feb132955052d6bcc068d2a380d5d74149c10d35
    
    * Revert to commit: c44b311
    
    * re-delete install-helm
    
    Co-authored-by: Dudi <dudicon@gmail.com>

--- publish.sh
+++ publish.sh
@@ -1,38 +1,42 @@
 #!/bin/sh
-set -e
-set -o pipefail
 
 WORKING_DIRECTORY="$PWD"
+GITHUB_PAGES_REPO=$1
+GITHUB_BRANCH=$2
+GITHUB_USERNAME=$3
+GITHUB_ACTIONS_REPO=$4
+GITHUB_ACTIONS_RUN_ID=$5
+GITHUB_USERNAME_TOKEN=$6
 
 [ "$GITHUB_PAGES_REPO" ] || {
   echo "ERROR: Environment variable GITHUB_PAGES_REPO is required"
   exit 1
 }
 
-[ "$CIRCLE_PULL_REQUEST" ] || {
-  echo "ERROR: Environment variable CIRCLE_PR_NUMBER is required"
+[ "$GITHUB_BRANCH" ] || {
+  echo "ERROR: Environment variable GITHUB_BRANCH is required"
   exit 1
 }
 
-# formating to number only
-PR_NUMBER=$(echo $CIRCLE_PULL_REQUEST | tr -dc '0-9')
-
-# Get labels from github-api and deserialize response using jq and sed
-LABELS=$(curl -s 'https://api.github.com/repos/'"${GITHUB_PAGES_REPO}"'/issues/'"${PR_NUMBER}"'/labels' | jq -r '.[] | .name' | sed 's/do not merge/do_not_merge/g') || {
-  echo "ERROR: curl failed to get response from github-api  /  failed to serialize data"
+[ "$GITHUB_USERNAME" ] || {
+  echo "ERROR: Environment variable GITHUB_USERNAME is required"
   exit 1
 }
 
-if [ -z "$LABELS" ]; then
-  echo "ERROR: Github-api failed to return answer / no labels found"
-  exit 1 
-fi
-
-# Using regex to detect if at least one proper label exist 
-if ! [[ "$LABELS" =~ .*"controller".* || "$LABELS" =~ .*"datastore".* || "$LABELS" =~ .*"operator".* || "$LABELS" =~ .*"global_change"*. ]]; then
-  echo "ERROR: Github-api failed to return answer / no proper labels found"
+[ "$GITHUB_ACTIONS_REPO" ] || {
+  echo "ERROR: Environment variable GITHUB_ACTIONS_REPO is required"
   exit 1
-fi
+}
+
+[ "$GITHUB_ACTIONS_RUN_ID" ] || {
+  echo "ERROR: Environment variable GITHUB_ACTIONS_RUN_ID is required"
+  exit 1
+}
+
+[ "$GITHUB_USERNAME_TOKEN" ] || {
+  echo "ERROR: Environment variable GITHUB_USERNAME_TOKEN is required"
+  exit 1
+}
 
 [ -z "$GITHUB_PAGES_BRANCH" ] && GITHUB_PAGES_BRANCH=gh-pages
 [ -z "$HELM_CHARTS_SOURCE" ] && HELM_CHARTS_SOURCE="$WORKING_DIRECTORY/charts"
@@ -40,30 +44,28 @@ fi
   echo "ERROR: Could not find Helm charts in $HELM_CHARTS_SOURCE"
   exit 1
 }
-[ -z "$HELM_VERSION" ] && HELM_VERSION=2.8.1
-[ "$CIRCLE_BRANCH" ] || {
-  echo "ERROR: Environment variable CIRCLE_BRANCH is required"
+[ -z "$HELM_VERSION" ] && HELM_VERSION=2.16.7
+[ "$GITHUB_BRANCH" ] || {
+  echo "ERROR: Environment variable GITHUB_BRANCH is required"
   exit 1
 }
 
-echo "LABELS=$LABELS"
-echo "PR_NUMBER=$PR_NUMBER"
 echo "GITHUB_PAGES_REPO=$GITHUB_PAGES_REPO"
 echo "GITHUB_PAGES_BRANCH=$GITHUB_PAGES_BRANCH"
 echo "HELM_CHARTS_SOURCE=$HELM_CHARTS_SOURCE"
 echo "HELM_VERSION=$HELM_VERSION"
-echo "CIRCLE_BRANCH=$CIRCLE_BRANCH"
+echo "GITHUB_BRANCH=$GITHUB_BRANCH"
+echo "GITHUB_ACTIONS_REPO=$GITHUB_ACTIONS_REPO"
+echo "GITHUB_ACTIONS_RUN_ID=$GITHUB_ACTIONS_RUN_ID"
 
 echo ">> Checking out $GITHUB_PAGES_BRANCH branch from $GITHUB_PAGES_REPO"
 cd /tmp/helm/publish
 mkdir -p "$HOME/.ssh"
-ssh-keyscan -H github.com >> "$HOME/.ssh/known_hosts"
-git clone -b "$GITHUB_PAGES_BRANCH" "git@github.com:$GITHUB_PAGES_REPO.git" .
-
-alias helm=/tmp/helm/bin/linux-amd64/helm
+git clone -b "${GITHUB_PAGES_BRANCH}" "https://${GITHUB_USERNAME}:${GITHUB_USERNAME_TOKEN}@github.com:/${GITHUB_ACTIONS_REPO}.git"
+cd helm-charts/
 
 echo '>> Building charts...'
-find "$HELM_CHARTS_SOURCE" -mindepth 1 -maxdepth 1 -type d | while read chart; do
+sudo find "$HELM_CHARTS_SOURCE" -mindepth 1 -maxdepth 1 -type d | while read chart; do
   chart_name="`basename "$chart"`"
   echo ">>> fetching chart $chart_name version"
   chart_version=$(cat $chart/Chart.yaml | grep -oE "version:\s[0-9]+\.[0-9]+\.[0-9]+" | grep -oE "[0-9]+\.[0-9]+\.[0-9]+")
@@ -73,17 +75,10 @@ find "$HELM_CHARTS_SOURCE" -mindepth 1 -maxdepth 1 -type d | while read chart; d
   mkdir -p "$chart_name"
   helm package -d "$chart_name" "$chart"
 done
-
 echo '>>> helm repo index'
 helm repo index .
-if [ "$CIRCLE_BRANCH" != "master" ]; then
+if [ "$GITHUB_BRANCH" != "refs/heads/master" ]; then
   echo "Current branch is not master and do not publish"
   exit 0
 fi
-echo ">> Publishing to $GITHUB_PAGES_BRANCH branch of $GITHUB_PAGES_REPO"
-git config user.email "$CIRCLE_USERNAME@users.noreply.github.com"
-git config user.name CircleCI
-git add .
-git status
-git commit -m "Published by CircleCI $CIRCLE_BUILD_URL"
-git push origin "$GITHUB_PAGES_BRANCH"
+
--- validate.sh
+++ validate.sh
@@ -1,21 +1,19 @@
-#!/bin/sh
-set -e
-set -o pipefail
+#!/bin/bash
 
 WORKING_DIRECTORY="$PWD"
+GITHUB_PAGES_REPO=$1
+PR_NUMBER=$2
+GITHUB_BRANCH=$3
 
 [ "$GITHUB_PAGES_REPO" ] || {
   echo "ERROR: Environment variable GITHUB_PAGES_REPO is required"
   exit 1
 }
 
-[ "$CIRCLE_PULL_REQUEST" ] || {
-  echo "ERROR: Environment variable CIRCLE_PULL_REQUEST is required"
-  exit 1
+[ "$PR_NUMBER" ] || {
+  echo "ERROR: Environment variable PR_NUMBER is required"
+  exit 0
 }
-
-# formating to number only
-PR_NUMBER=$(echo $CIRCLE_PULL_REQUEST | tr -dc '0-9')
 
 # Get labels from github-api and deserialize response using jq and sed
 LABELS=$(curl -s 'https://api.github.com/repos/'"${GITHUB_PAGES_REPO}"'/issues/'"${PR_NUMBER}"'/labels' | jq -r '.[] | .name' | sed 's/do not merge/do_not_merge/g') || {
@@ -28,21 +26,15 @@ if [ -z "$LABELS" ]; then
   exit 1 
 fi
 
-# Using regex to detect if at least one proper label exist 
-if ! [[ "$LABELS" =~ .*"controller".* || "$LABELS" =~ .*"datastore".* || "$LABELS" =~ .*"operator".* || "$LABELS" =~ .*"global_change"*. ]]; then
-  echo "ERROR: Github-api failed to return answer / no proper labels found"
-  exit 1
-fi
-
 [ -z "$GITHUB_PAGES_BRANCH" ] && GITHUB_PAGES_BRANCH=gh-pages
 [ -z "$HELM_CHARTS_SOURCE" ] && HELM_CHARTS_SOURCE="$WORKING_DIRECTORY/charts"
 [ -d "$HELM_CHARTS_SOURCE" ] || {
   echo "ERROR: Could not find Helm charts in $HELM_CHARTS_SOURCE"
   exit 1
 }
-[ -z "$HELM_VERSION" ] && HELM_VERSION=2.8.1
-[ "$CIRCLE_BRANCH" ] || {
-  echo "ERROR: Environment variable CIRCLE_BRANCH is required"
+[ -z "$HELM_VERSION" ] && HELM_VERSION=2.16.7
+[ "$GITHUB_BRANCH" ] || {
+  echo "ERROR: Environment variable GITHUB_BRANCH is required"
   exit 1
 }
 
@@ -52,26 +44,24 @@ echo "GITHUB_PAGES_REPO=$GITHUB_PAGES_REPO"
 echo "GITHUB_PAGES_BRANCH=$GITHUB_PAGES_BRANCH"
 echo "HELM_CHARTS_SOURCE=$HELM_CHARTS_SOURCE"
 echo "HELM_VERSION=$HELM_VERSION"
-echo "CIRCLE_BRANCH=$CIRCLE_BRANCH"
+echo "GITHUB_BRANCH=$GITHUB_BRANCH"
 
 echo ">> Checking out $GITHUB_PAGES_BRANCH branch from $GITHUB_PAGES_REPO"
 cd /tmp/helm/publish
 mkdir -p "$HOME/.ssh"
-ssh-keyscan -H github.com >> "$HOME/.ssh/known_hosts"
-git clone -b "$GITHUB_PAGES_BRANCH" "git@github.com:$GITHUB_PAGES_REPO.git" .
-
-alias helm=/tmp/helm/bin/linux-amd64/helm
+git clone -b "${GITHUB_PAGES_BRANCH}" "https://github.com/${GITHUB_PAGES_REPO}.git"
+cd helm-charts/
 
 echo '>> Building charts and comparing with labels...'
-find "$HELM_CHARTS_SOURCE" -mindepth 1 -maxdepth 1 -type d | while read chart; do
+sudo find "$HELM_CHARTS_SOURCE" -mindepth 1 -maxdepth 1 -type d | while read chart; do
   chart_name="`basename "$chart"`"
   for label in $LABELS; do
-  if [ $label == $chart_name ]; then
+  if [ $label = $chart_name ]; then
     echo ">>> fetching chart $chart_name version"
     chart_version=$(cat $chart/Chart.yaml | grep -oE "version:\s[0-9]+\.[0-9]+\.[0-9]+" | grep -oE "[0-9]+\.[0-9]+\.[0-9]+")
     echo ">>> checking if version is already published"
     if [ -f "$chart_name/$chart_name-$chart_version.tgz" ]; then
-      echo ">>> Error: VERSION $chart_version ALREADY EXISTS! Update chart version."
+      echo ">>> Error: CHART $chart_name VERSION $chart_version ALREADY EXISTS! Update chart version."
       exit 1
     else
       echo ">>> chart version is valid, continuing..."
